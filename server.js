4e41466c2757584658b4a8bc2b309e9f997af5a7
Jayesh Kusuma <jayeshkusuma77@gmail.com>
2021-08-10 09:41:29 +0530

    User Authentication API Endpoint

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const app = express();
 const db = require('./db');
-const roomsRoute= require("./routes/roomsRoute");
 const port = process.env.PORT||7000;
+const roomsRoute= require("./routes/roomsRoute");
+const userRoute = require('./routes/userRoute')
+const bookingsRoute=require('./routes/bookingsRoute')
+app.use(express.json());
 
 //Routes
 app.use('/api/rooms',roomsRoute);
+app.use('/api/users' , userRoute)
+app.use('/api/bookings' , bookingsRoute)
 
 app.listen(port, () => {
     console.log("Backend server is running!");
